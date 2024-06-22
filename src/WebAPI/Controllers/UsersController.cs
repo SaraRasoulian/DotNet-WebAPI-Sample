d953194fc87d2e014c5f41906b075e28f11d97ba
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-06-22 18:21:17 +0330

    Implement EarnPoints api

---
+++ src/WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Application.RequestModels;
+using Application.Users.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/users")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    //[Authorize]
+    [HttpPost("{id:long}/earn")]
+    public async Task<IActionResult> EarnPoints(long id, [FromBody] EarnPointsRequest request)
+    {
+        var command = new EarnPointsCommand(id, request.Points);
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
