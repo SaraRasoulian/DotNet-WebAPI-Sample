ee8eb600a7229ba441b45f65b7c5e31a089ec0ba
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-06-22 13:57:27 +0330

    Implement user entity

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Domain.ValueObjects;
+using Domain.Common;
+
+namespace Domain.Entities;
+
+public record User : EntityBase
+{
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string UserName { get; set; } = null!;
+
+    public Email Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public decimal PointBalance { get; set; }    
+}
