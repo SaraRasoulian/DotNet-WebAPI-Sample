473f6edf2e0350746a30b8bf989c28c5f2acfafa
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-06-23 14:10:32 +0330

    Set up WebApplicationFactory class for integration testing

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -62,3 +62,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
---
+++ tests/Application.Tests.Integration/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Infrastructure.DbContexts;
+using Testcontainers.PostgreSql;
+
+namespace Application.Tests.Integration;
+
+public class IntegrationTestWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("LoyaltyDB_Test")
+        .WithUsername("postgres")
+        .WithPassword("mysecretpassword")
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+
+        using (var scope = Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            var cntx = scopedServices.GetRequiredService<LoyaltyDBContext>();
+
+            await cntx.Database.EnsureCreatedAsync();
+        }
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<LoyaltyDBContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<LoyaltyDBContext>(options =>
+            {
+                options.UseNpgsql(_dbContainer.GetConnectionString());
+            });
+        });
+    }
+}
