ac94f24aaeb7df8077bc4cee94be2922a032f6fd
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-06-23 15:16:05 +0330

    Add integration test for earn point api

---
+++ tests/WebAPI.Tests.Integration/BaseControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using Application.RequestModels;
+using Application.Tests.Integration;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace WebAPI.Tests.Integration;
+
+public class BaseControllerTest : IClassFixture<IntegrationTestWebApplicationFactory>
+{
+    protected readonly HttpClient _httpClient;
+
+    protected BaseControllerTest(IntegrationTestWebApplicationFactory factory)
+    {
+        _httpClient = factory.CreateDefaultClient();
+    }
+
+    /// <summary>
+    /// Authenticate using the one user created by database seed
+    /// </summary>
+    /// <returns></returns>
+    protected async Task AuthenticateAsync()
+    {
+        LoginRequest loginRequest = new LoginRequest
+        (
+            UserName: "sara",
+            Password: "123456"
+        );
+
+        var loginResponse = await _httpClient.PostAsJsonAsync("api/identity/login", loginRequest);
+        string token = await loginResponse.Content.ReadAsStringAsync();
+
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+}
---
+++ tests/WebAPI.Tests.Integration/ControllersTests/UsersControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Application.RequestModels;
+using Application.Tests.Integration;
+using System.Net.Http.Json;
+using FluentAssertions;
+
+namespace WebAPI.Tests.Integration.ControllersTests;
+
+public class UsersControllerTests : BaseControllerTest
+{
+    public UsersControllerTests(IntegrationTestWebApplicationFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task EarnPoints_Returns_OK_With_PointBalance()
+    {
+        // Arrange
+        await AuthenticateAsync();
+        var userId = 1;
+        EarnPointsRequest request = new EarnPointsRequest(Points: 100);
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync("/api/users/" + userId + "/earn", request);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        decimal pointBalance = decimal.Parse(await response.Content.ReadAsStringAsync());
+        pointBalance.Should().BeGreaterThan(0);
+    }
+}
