5e5fc623dac4c576b1da27acaf18e64ac16debb0
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-06-23 14:11:36 +0330

    Add integration test for earn points command/handler

---
+++ tests/Application.Tests.Integration/UserHandlersTests/UserHandlersTests.cs
@@ -0,0 +1,24 @@
+﻿using Application.Users.Commands;
+using FluentAssertions;
+
+namespace Application.Tests.Integration.UserHandlersTests;
+
+public class UserHandlersTests : BaseHandlerTest
+{
+    public UserHandlersTests(IntegrationTestWebApplicationFactory factory) : base(factory) { }
+
+    [Fact]
+    public async void EarnPointsCommand_Returns_PointsBalance()
+    {
+        // Arrange
+        var command = new EarnPointsCommand(
+            UserId: 1,
+            Points: 200);
+
+        // Act
+        var result = await _sender.Send(command);
+
+        // Assert
+        result.Should().BeGreaterThan(0);        
+    }
+}
