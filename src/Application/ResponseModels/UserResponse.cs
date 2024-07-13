b8d996b30af1f473f4435e40ce4300082a8e108f
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-07-13 12:56:53 +0330

    Add userResponse record

---
+++ src/Application/ResponseModels/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿using Domain.ValueObjects;
+
+namespace Application.ResponseModels;
+
+public record UserResponse(
+    long Id,
+    string FirstName,
+    string LastName,
+    string UserName,
+    Email Email,
+    decimal PointBalance);
