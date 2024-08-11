ba93d51a48e1ec114148bea514026c3ac84831da
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-08-11 14:12:59 +0330

    Add integration test for GetUserHandler

---
+++ tests/Application.Tests.Integration/UserHandlersTests/GetUserHandlerTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Tests.Integration.UserHandlersTests;
+
+public class GetUserHandlerTests : BaseHandlerTest
+{
+    public GetUserHandlerTests(IntegrationTestWebApplicationFactory factory) : base(factory) { }
+
+    [Fact]
+    public async void GetUserHandler_Returns_User()
+    {
+        // Arrange
+        var query = new GetUserQuery(UserId: 1);
+
+        // Act
+        var result = await _sender.Send(query);
+
+        // Assert
+        result.Id.Should().Be(query.UserId);
+    }
+}
